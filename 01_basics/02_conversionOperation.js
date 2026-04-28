fb4743948ed6463f1cd0b3189366eff37631b221
Ankit Yadav <ankityadaw856@gmail.com>
2026-04-28 06:40:35 +0000

    Conversion of datatypes

---
+++ 01_basics/02_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33" 
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// "true" => 1
+
+
+let isLoggedIn = 0
+
+let booleanIsLogged = Boolean(isLoggedIn)
+console.log(booleanIsLogged);
+
+// 1 => true; 0 => false
+// "" => false
+// "xyz" => true
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
